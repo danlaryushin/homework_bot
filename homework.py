22f9f738b50a28c1f8824e397a038f8928de8b93
Даниил Ларюшин <danlaryushin@gmail.com>
2022-09-15 13:36:28 +0300

    HelpBot

--- homework.py
+++ homework.py
@@ -1,5 +1,6 @@
 import logging
 import os
+import sys
 import time
 from http import HTTPStatus
 
@@ -15,7 +16,7 @@ logger.setLevel(logging.DEBUG)
 formatter = logging.Formatter(
     '%(asctime)s - %(funcName)s - [%(levelname)s] - %(message)s'
 )
-handler = logging.StreamHandler()
+handler = logging.StreamHandler(sys.stdout)
 handler.setFormatter(formatter)
 logger.addHandler(handler)
 
